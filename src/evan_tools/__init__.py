da5b2617e9a27530a4d4a2d0c037e33325c69881
EvanstonLaw <kaluoshilong@qq.com>
2025-12-05 03:16:16 +0800

    feat(init): 更新__all__导出，添加MD5计算相关功能

--- src/evan_tools/__init__.py
+++ src/evan_tools/__init__.py
@@ -1,4 +1,13 @@
 from .file import gather_paths
+from .md5 import MD5Result, calc_full_md5, calc_sparse_md5
 from .setup import AutoDeployer, ProjectConfig, run_deployer
 
-__all__ = ["gather_paths", "ProjectConfig", "AutoDeployer", "run_deployer"]
+__all__ = [
+    "gather_paths",
+    "ProjectConfig",
+    "AutoDeployer",
+    "run_deployer",
+    "calc_sparse_md5",
+    "MD5Result",
+    "calc_full_md5",
+]
